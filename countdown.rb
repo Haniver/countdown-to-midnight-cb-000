5fca66f795590d7cbcff203d8fbbeab814c3723e
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-03 15:37:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,3 @@
-#write your code here
-
 def countdown(number)
   while number > 0
     puts"#{number} SECOND(S)!"
@@ -7,3 +5,13 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts"#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
